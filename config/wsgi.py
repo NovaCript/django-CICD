25a7dcf05ed5858d42ea03cf9ebc4763ff7808a5
Gleb <114811823+NovaCript@users.noreply.github.com>
2024-03-28 11:46:59 +0300

    improve

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,23 @@
 """
 WSGI config for config project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGi callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add the project directory to the sys.path
+project_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(project_dir)
+sys.path.insert(0, project_root)
 
+# Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+# Import and instantiate the Django WSGI application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
