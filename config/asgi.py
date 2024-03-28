3173ab6cb47e9e3041cb1d6b2a284168e3579243
Gleb <114811823+NovaCript@users.noreply.github.com>
2024-03-28 11:46:58 +0300

    improve

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,15 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+# Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+# Get the ASGI application
 application = get_asgi_application()
+
+# If the ASGI_SERVER environment variable is set to Daphne, wrap the application
+# in Daphne's ASGI handler
+if os.environ.get("ASGI_SERVER") == "Daphne":
+    from daphne.www.application import WSGIApplication
+
+    application = WSGIApplication(application)
